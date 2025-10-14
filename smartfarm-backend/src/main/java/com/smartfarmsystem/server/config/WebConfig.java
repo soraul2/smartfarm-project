fbc05ee61bb185d2c494889f91295af6f39c09fc
김태우 <goxodn1@gmail.com>
2025-10-15 05:44:20 +0900

    <update>(web config) : webconfig 에서 외부 ip를 등록을 해주지 않아서 프런트에서 백엔드로 데이터가 정상적으로 가지 않고 있기에 해당 외부 ip를 추가해줌

--- smartfarm-backend/src/main/java/com/smartfarmsystem/server/config/WebConfig.java
+++ smartfarm-backend/src/main/java/com/smartfarmsystem/server/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트엔드 출처 허용
+                .allowedOrigins("http://localhost:3000",
+                "http://34.53.118.212:3000"
+                ) // 프론트엔드 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP method
                 .allowedHeaders("*"); // 모든 헤더 허용
     }
